f887d6e761e5f5a4ca8b256cf2818dcb63c0847f
Jackson Gray Morgan <jacksonmo@me.com>
2020-01-02 18:04:26 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -15,6 +15,13 @@ end
 
 introduction_with_language("Jackson", "Ruby")
 
+def introduction_with_language_optional(name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+end
+
+introduction_with_language_optional("Jackson")
+
+
 
 
 
